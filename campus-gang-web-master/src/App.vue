431f9883371b579576dde1cc5abe1c4bb98ed09f
peak <516665061@qq.com>
2024-05-04 15:59:25 +0800

    项目第二十四次提交，修复富文本覆盖下拉框的问题

--- campus-gang-web-master/src/App.vue
+++ campus-gang-web-master/src/App.vue
@@ -3,3 +3,9 @@
     <router-view/>
   </div>
 </template>
+
+ <style>
+ .w-e-menu { z-index: 2 !important; }
+ .w-e-text-container { z-index: 1 !important; }
+ .w-e-toolbar { z-index: 2 !important; }
+ </style>
